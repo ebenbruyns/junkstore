166073885a7ed603252981a586243048d0308f84
Beebles <102569435+beebls@users.noreply.github.com>
2023-10-16 17:01:48 -0600

    test - don't use global dfl

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,15 @@
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import replace from '@rollup/plugin-replace';
-import typescript from '@rollup/plugin-typescript';
-import { defineConfig } from 'rollup';
-import importAssets from 'rollup-plugin-import-assets';
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+import typescript from "@rollup/plugin-typescript";
+import { defineConfig } from "rollup";
+import importAssets from "rollup-plugin-import-assets";
 
 import { name } from "./plugin.json";
 
 export default defineConfig({
-  input: './src/index.tsx',
+  input: "./src/index.tsx",
   plugins: [
     commonjs(),
     nodeResolve(),
@@ -17,22 +17,21 @@ export default defineConfig({
     json(),
     replace({
       preventAssignment: false,
-      'process.env.NODE_ENV': JSON.stringify('production'),
+      "process.env.NODE_ENV": JSON.stringify("production"),
     }),
     importAssets({
-      publicPath: `http://127.0.0.1:1337/plugins/${name}/`
-    })
+      publicPath: `http://127.0.0.1:1337/plugins/${name}/`,
+    }),
   ],
-  context: 'window',
-  external: ["react", "react-dom", "decky-frontend-lib"],
+  context: "window",
+  external: ["react", "react-dom"],
   output: {
     file: "dist/index.js",
     globals: {
       react: "SP_REACT",
       "react-dom": "SP_REACTDOM",
-      "decky-frontend-lib": "DFL"
     },
-    format: 'iife',
-    exports: 'default',
+    format: "iife",
+    exports: "default",
   },
 });
