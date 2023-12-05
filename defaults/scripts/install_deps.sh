f048d42025d9e66a54c879b09f60512a7fdd1941
Eben Bruyns <eben@sdk.co.nz>
2023-12-05 18:02:32 +1300

    install dependencies first, then legendary

--- defaults/scripts/install_deps.sh
+++ defaults/scripts/install_deps.sh
@@ -3,6 +3,7 @@ if flatpak list | grep -q "com.github.derrod.legendary"; then
     echo "legendary flatpak is installed"
 else
  cd /tmp
+    flatpak install flathub org.gnome.Platform//45
     wget https://github.com/ebenbruyns/legendary-flatpak/releases/download/Test-0.1/legendary.flatpak
     flatpak --user install legendary.flatpak -y
     rm legendary.flatpak
