2c4ffe5c02fb37d05ac64c48c28d073478d101f6
Eben Bruyns <eben@sdk.co.nz>
2023-12-16 08:38:31 +1300

    Updated flatpak to resolve login issue.

--- defaults/scripts/install_deps.sh
+++ defaults/scripts/install_deps.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 if flatpak list | grep -q "com.github.derrod.legendary"; then
-    echo "legendary flatpak is installed"
-else
- cd /tmp
-    flatpak install flathub org.gnome.Platform//45 -y
-    wget https://github.com/ebenbruyns/legendary-flatpak/releases/download/Test-0.1/legendary.flatpak
+    echo "legendary flatpak is installed, removing and reinstalling"
+    flatpak uninstall com.github.derrod.legendary -y
+    cd /tmp
+    flatpak --user install flathub org.gnome.Platform//45 -y
+    wget https://github.com/ebenbruyns/legendary-flatpak/releases/download/Test-0.2/legendary.flatpak
     flatpak --user install legendary.flatpak -y
     rm legendary.flatpak
-fi
+else
+    cd /tmp
+    flatpak --user install flathub org.gnome.Platform//45 -y
+    wget https://github.com/ebenbruyns/legendary-flatpak/releases/download/Test-0.2/legendary.flatpak
+    flatpak --user install legendary.flatpak -y
+    rm legendary.flatpakfi
 
 echo "==================================="
 echo "  Dependecy installation complete"
