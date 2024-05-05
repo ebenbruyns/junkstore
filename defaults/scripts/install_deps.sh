a7b7883c267249b527eabe0c75dcdf1f97d0036f
Eben Bruyns <eben@sdk.co.nz>
2024-05-06 08:49:05 +1200

    Adding more user feedback on install dependencies

--- defaults/scripts/install_deps.sh
+++ defaults/scripts/install_deps.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
+function install(){ 
+    echo "==================================="
+    echo "  Dependecy installation starting"
+    echo "  Do not navigate away please..."
+    echo "==================================="
+    #recusively find all install_deps.sh files and execute them
+    find ./scripts/Extensions -type f -name "install_deps.sh" -exec bash {} \;
+    find ~/homebrew/data/Junk-Store/scripts/Extensions -type f -name "install_deps.sh" -exec bash {} \;
 
-#recusively find all install_deps.sh files and execute them
-find ./scripts/Extensions -type f -name "install_deps.sh" -exec bash {} \;
-find ~/homebrew/data/Junk-Store/scripts/Extensions -type f -name "install_deps.sh" -exec bash {} \;
 
 
+    echo "==================================="
+    echo "  Dependecy installation complete"
+    echo "==================================="
+}
 
-echo "==================================="
-echo "  Dependecy installation complete"
-echo "==================================="
+function uninstall(){
+    echo "==================================="
+    echo "  Dependecy uninstallation starting"
+    echo "  Do not navigate away please..."
+    echo "==================================="
+    #recusively find all install_deps.sh files and execute them
+    find ./scripts/Extensions -type f -name "install_deps.sh" -exec bash {} uninstall \;
+    find ~/homebrew/data/Junk-Store/scripts/Extensions -type f -name "install_deps.sh" -exec bash {} uninstall \;
+
+    echo "==================================="
+    echo "  Dependecy uninstallation complete"
+    echo "==================================="
+}
+
+if [ "$1" == "uninstall" ]; then
+    uninstall
+else
+    install
+fi
