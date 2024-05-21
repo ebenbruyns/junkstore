e744174a578438e82f68aaa28c06a284ffb15014
Eben Bruyns <eben@sdk.co.nz>
2024-05-21 15:35:38 +1200

    Added a branch switcher for dev downloads

--- src/Developer.tsx
+++ src/Developer.tsx
@@ -1,9 +1,10 @@
-import { ConfirmModal, DialogButton, ModalRoot, PanelSection, ScrollPanelGroup, ServerAPI, TextField, ToggleField, showModal } from "decky-frontend-lib";
+import { ConfirmModal, DialogButton, Dropdown, ModalRoot, PanelSection, ScrollPanelGroup, ServerAPI, TextField, ToggleField, showModal } from "decky-frontend-lib";
 import { VFC, useEffect, useState } from "react";
 import { resetAchievements } from "./Utils/achievements";
 
 import Logger from "./Utils/logger";
 import { FaDownload } from "react-icons/fa6";
+import { get } from "mobx";
 export interface BuildDownloaderProps {
     serverAPI: ServerAPI;
     closeModal?: any;
@@ -13,11 +14,11 @@ export const BuildDownloader: VFC<BuildDownloaderProps> = ({ serverAPI, closeMod
     const [builds, setBuilds] = useState([]);
     const [value, setValue] = useState("");
     const [bearerToken, setBearerToken] = useState(localStorage.getItem('js_gh_bearerToken') || "");
-
+    const [branch, setBranch] = useState(localStorage.getItem('js_gh_branch') || "main");
     const get_builds = async () => {
 
         const res = await serverAPI.callPluginMethod<{}, {}>("get_latest_builds", {
-            branch: "main",
+            branch: branch,
             bearer_token: bearerToken
         });
         logger.log("get_latest_builds", res);
@@ -33,23 +34,34 @@ export const BuildDownloader: VFC<BuildDownloaderProps> = ({ serverAPI, closeMod
         setValue(value);
         localStorage.setItem('js_gh_bearerToken', value);
     }
+    useEffect(() => {
+        get_builds();
+    }, [branch])
+     
+    const changeBranch = async (value: string) => {
+        setBranch(value);
+        localStorage.setItem('js_gh_branch', value);
+        
+    }
     return (
         <ModalRoot
 
             bAllowFullSize={true}
             closeModal={closeModal}
         >
-            <DialogButton onClick={async () => {
-                if (bearerToken === "") {
-                    localStorage.setItem('js_gh_bearerToken', value);
-                    setBearerToken(value);
-                    get_builds();
-                    return;
-                }
-                setBearerToken("");
-                localStorage.setItem('js_gh_bearerToken', "");
-            }}
-            >{bearerToken === "" ? "Save Bearer Token" : "Clear Bearer Token"}</DialogButton>
+            <PanelSection>
+                <DialogButton onClick={async () => {
+                    if (bearerToken === "") {
+                        localStorage.setItem('js_gh_bearerToken', value);
+                        setBearerToken(value);
+                        get_builds();
+                        return;
+                    }
+                    setBearerToken("");
+                    localStorage.setItem('js_gh_bearerToken', "");
+                }}
+                >{bearerToken === "" ? "Save Bearer Token" : "Clear Bearer Token"}</DialogButton>
+            </PanelSection>
             {bearerToken === "" && <PanelSection>
                 <h2>Enter Github Token</h2>
                 <TextField
@@ -60,6 +72,22 @@ export const BuildDownloader: VFC<BuildDownloaderProps> = ({ serverAPI, closeMod
                 />
             </PanelSection>
             }
+            <PanelSection>
+                <Dropdown
+                    menuLabel="Branch"
+                    selectedOption={branch}
+                    rgOptions={[{
+                        data: "main",
+                        label: "Stable",
+                    },
+                    {
+                        data: "development",
+
+                        label: "Development",
+                    },]}
+                    onChange={(e) => changeBranch(e.data)}
+                />
+            </PanelSection>
             <div>
                 <p>Latest Builds</p>
 
@@ -67,10 +95,10 @@ export const BuildDownloader: VFC<BuildDownloaderProps> = ({ serverAPI, closeMod
                     builds.map((build: any) => {
                         return (
                             <>
-                                <div style={{paddingBottom:"10px"}}>
+                                <div style={{ paddingBottom: "10px" }}>
                                     <div style={{ display: "flex", gap: '15px' }}>
                                         <div style={{ width: '100%' }}>Junk-Store-{build.head_sha}.zip on {build.date} <br />
-                                        {build.title}</div>
+                                            {build.title}</div>
                                         <DialogButton
                                             style={{ width: "48px", height: "48px", minWidth: 'initial', padding: 'initial' }}
                                             onClick={async () => {
@@ -87,7 +115,7 @@ export const BuildDownloader: VFC<BuildDownloaderProps> = ({ serverAPI, closeMod
                                             <FaDownload />
                                         </DialogButton>
                                     </div>
-                                    
+
                                 </div>
 
 
